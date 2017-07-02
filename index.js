6485a6961f2cb418f409bade55d314182a9ea735
FireFlyForLife <maiko.s.99@gmail.com>
2017-07-02 19:02:45 +0200

    message_parser.js and commands.js should now do something, not tested

--- index.js
+++ index.js
@@ -2,30 +2,15 @@
 var tmi = require('tmi.js');
 var config = require('./config.js');
 var user = require('./src/core/accounts/user.js');
-
-//These are the settings for the client to use.
-var options = {
-  options: {
-    debug: true
-  },
-
-  connection: {
-    cluster: "aws",
-    reconnect: true
-  },
-
-  identity: {
-    username: "brentolinni",
-    password: "oauth:3mr7wbq778ezrw2u6excq0ntv7fnkr"
-  },
-  channels: ["BRENTOLINNI"]
-};
+var message_parser = require('./src/core/accounts/user.js');
 
 //This is creating our client connection with settings.
 var client = new tmi.client(config.options);
 
 //This connects to the twitch.
 client.connect();
+
+message_parser.init(client);
 
 //This function is executed as soon as the bot has connected to the channel.
 client.on("connected", function(address, port){
@@ -37,11 +22,13 @@ client.on("chat", function(channel, userstate, message){
   //Using this if statement you can check the contents of a message and create commands.
   //This checks the contents of the message to see if they match the given message, this means that the message HAS to be that it cannot just contain that message.
   //This allows you to set commands or even words without worrying if someone was to use the word in a sentence or so on.
-  if(message === "!add"){
-    if(user.check(userstate)){
-      user.add(usersstate);
-    };
-  }
+  
+  // This is now in message_parser
+  // if(message === "!add"){
+  //   if(user.check(userstate)){
+  //     user.add(usersstate);
+  //   };
+  // }
 
 });
 
