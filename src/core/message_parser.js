454e11401ba217e1a9e9a7392d90f34bc6b7dce1
FireFlyForLife <maiko.s.99@gmail.com>
2017-07-02 23:38:09 +0200

    some documentation

--- src/core/message_parser.js
+++ src/core/message_parser.js
@@ -2,9 +2,9 @@ var user = require("./accounts/user.js");
 var commands = require("./commands/command.js");
 
 var message_parser = {};
+module.exports = message_parser;
 
 //aliases
-module.exports = message_parser;
 var mp = message_parser;
 
 //variable declarations
@@ -15,6 +15,12 @@ message_parser.init = function(client){
 	client.on('chat', mp.onChat);
 }
 
+/**
+ * @param channel {string}
+ * @param userstate {object} the userstate object https://docs.tmijs.org/v1.2.1/Events.html#chat
+ * @param message {string} 
+ * @param self {boolean} is the command send out by this bot?
+ */
 message_parser.onChat = function(channel, userstate, message, self){
 	if(self)
 		return;
@@ -30,6 +36,11 @@ message_parser.onChat = function(channel, userstate, message, self){
 	}
 };
 
+/**
+ * plits up the chat message into usable pieces for the command system
+ * @param message {string} 
+ * @return {object} with the properties: 'commmand' and 'args'
+ */
 message_parser.prepareCommand = function(message){
 	var full_command = message.substring(mp.prefix.length);
 	var args = full_command.split(' ');
