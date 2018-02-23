4a65296d058fe201ac31b1bd815d0d31b8dcfca8
Brent Minkes <brentminkes@hotmail.com>
2018-02-23 17:36:10 +0100

    added mixer, small battle test

---
+++ src/TwitchRPG/Assets/BattleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        MixerInteractive.Initialize(true);
+        MixerInteractive.GoInteractive();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MixerInteractive.GetButton("attack"))
+        {
+            Debug.Log("Player Attacked");
+        }
+    }
+}
